0b1de7af3a8e44ba8f1a2b15f5d6ad100a60d5fe
Christian Stachowitz <companion@stagetec.com>
2025-03-31 15:09:31 +0200

    prettier fix for ".." to "->" SPACE to TABS
    prettier fix for ".." to "->" SPACE to TABS

--- src/presets.ts
+++ src/presets.ts
@@ -1,6 +1,6 @@
 import { CompanionPresetDefinitions } from '@companion-module/base'
 
 export function GetPresetsList(): CompanionPresetDefinitions {
-  const presets: CompanionPresetDefinitions = {}
-  return presets
+	const presets: CompanionPresetDefinitions = {}
+	return presets
 }
--- src/variables.ts
+++ src/variables.ts
@@ -2,10 +2,10 @@ import { CompanionVariableDefinition } from '@companion-module/base'
 import { EmberPlusConfig } from './config'
 
 export function GetVariablesList(config: EmberPlusConfig): CompanionVariableDefinition[] {
-  return (
-    config.monitoredParameters?.map(({id,label}) => ({
-      name: id,
-      variableId: label,
-    })) ?? []
-  )
+	return (
+		config.monitoredParameters?.map(({id,label}) => ({
+			name: id,
+			variableId: label,
+		})) ?? []
+	)
 }
